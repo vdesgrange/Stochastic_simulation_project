7162abe063cbc5f7bb8aa3b3df144d0c53d4d733
Viviane Desgrange <viviane.desgrange@gmail.com>
2020-11-01 13:33:15 +0100

    Add difference by number of sampling.

--- assignment_1/graphic_utils.py
+++ assignment_1/graphic_utils.py
@@ -1,11 +1,20 @@
 import matplotlib.pyplot as plt
 
 
-def difference_plot(x, y):
+def difference_plot_by_iteration(x, y):
     fig, ax = plt.subplots(dpi=300)
     ax.set_xlabel('Number of iterations j')
     ax.set_ylabel(r'$|A_{js} - A_{is}|$')
     ax.set_title(r'Evolution of $|A_{js} - A_{is}|,\ \forall j < i$')
+    ax.loglog(x, y, color='coral', linewidth='.5')
+    plt.show()
+
+
+def difference_plot_by_sampling(x, y):
+    fig, ax = plt.subplots(dpi=300)
+    ax.set_xlabel('Number of sampling t')
+    ax.set_ylabel(r'$|A_{it} - A_{is}|$')
+    ax.set_title(r'Evolution of $|A_{it} - A_{is}|,\ \forall t < s$')
     ax.loglog(x, y, color='coral', linewidth='.5')
     plt.show()
 
