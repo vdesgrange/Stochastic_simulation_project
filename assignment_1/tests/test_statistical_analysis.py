45cf83262f9e1b80ee7f498215ab25d7d80debb8
Viviane Desgrange <viviane.desgrange@gmail.com>
2020-11-07 23:14:31 +0100

    wip.

---
+++ assignment_1/tests/test_statistical_analysis.py
@@ -0,0 +1,26 @@
+import unittest
+import numpy as np
+from .. import statistical_analysis
+
+class MyTestCase(unittest.TestCase):
+    def test_sample_mean(self):
+        x = np.arange(1, 6, 1)
+        self.assertEqual(statistical_analysis.sample_mean(x), 3.)
+
+    def test_sample_variance(self):
+        x = np.arange(1, 6, 1)
+        self.assertEqual(statistical_analysis.sample_variance(x), 2.5)
+
+        x = np.array([170, 300, 430, 470, 600])
+        self.assertEqual(statistical_analysis.sample_variance(x), 27130)
+
+    def test_sample_standard_deviation(self):
+        x = np.arange(1, 6, 1)
+        self.assertEqual(statistical_analysis.sample_standard_deviation(x), np.sqrt(10/4))
+
+        x = np.array([170, 300, 430, 470, 600])
+        self.assertEqual(statistical_analysis.sample_standard_deviation(x), np.sqrt(27130))
+
+
+if __name__ == '__main__':
+    unittest.main()
