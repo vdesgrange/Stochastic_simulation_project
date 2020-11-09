fa106373acda45f7e0f62e30482774cd72d1fa25
Viviane Desgrange <viviane.desgrange@gmail.com>
2020-11-09 01:53:19 +0100

    Change dpi and fix legend.

--- assignment_1/graphic_utils.py
+++ assignment_1/graphic_utils.py
@@ -8,7 +8,7 @@ palette = ['#060A0F', '#0B141E', '#111E2D', '#16283C', '#1C324A', '#213C59', '#2
 
 
 def difference_plot_by_iteration(x, y):
-    fig, ax = plt.subplots(dpi=300)
+    fig, ax = plt.subplots(dpi=150)
     ax.set_xlabel('Number of iterations j')
     ax.set_ylabel(r'$|A_{js} - A_{is}|$')
     ax.set_title(r'Evolution of $|A_{js} - A_{is}|,\ \forall j < i$')
@@ -17,7 +17,7 @@ def difference_plot_by_iteration(x, y):
 
 
 def difference_plot_by_sampling(x, y):
-    fig, ax = plt.subplots(dpi=300)
+    fig, ax = plt.subplots(dpi=150)
     ax.set_xlabel('Number of sampling t')
     ax.set_ylabel(r'$|A_{it} - A_{is}|$')
     ax.set_title(r'Evolution of $|A_{it} - A_{is}|,\ \forall t < s$')
@@ -26,7 +26,7 @@ def difference_plot_by_sampling(x, y):
 
 
 def convergence_plot_by_sampling_method(x_rand, y_rand, x_halton, y_halton, x_lhs, y_lhs):
-    fig, ax = plt.subplots(dpi=300)
+    fig, ax = plt.subplots(dpi=150)
 
     ax.set_xlabel('Number of sampling t')
     ax.set_ylabel(r'$|A_{it} - A_{is}|$')
@@ -43,16 +43,16 @@ def convergence_plot_by_sampling_method(x_rand, y_rand, x_halton, y_halton, x_lh
 
 
 def complex_plan_plot(re, im):
-    fig, ax = plt.subplots(dpi=300)
-    ax.set_xlabel('Re(A[n])')
-    ax.set_ylabel('Im(A[n])')
+    fig, ax = plt.subplots(dpi=150)
+    ax.set_xlabel('Re')
+    ax.set_ylabel('Im')
     ax.set_title(r'Convergence of complex number $c \approx {:3f} + ({:3f})i$'.format(re[0], im[0]))
     ax.plot(re, im, color='lightskyblue', marker='o', linewidth='.5')
     plt.show()
 
 
 def sampling_scatter_plot(x_samples, y_samples):
-    fig, ax = plt.subplots(dpi=300)
+    fig, ax = plt.subplots(dpi=150)
     ax.scatter(x_samples, x_samples, color='coral', s=0.5)
     plt.title('Sampling Scatter')
     ax.set_xlabel('X Sample')
